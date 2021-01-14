db338003c34261abe5293ab9564cb4f2b51e551a
Emmanuel Isidahomen <50160339+Emmanuelisi20@users.noreply.github.com>
2021-01-14 05:32:20 -0500

    Add files via upload
    added new files to learn

---
+++ java projects/Helloworld.java
@@ -0,0 +1,55 @@
+/**
+ * My first program in java Helloworld
+ * Emmanuel Isidahomen 
+ * 1/14/2021
+ * 
+ * Notes 
+ * *********************************************************************************************************************************
+ * 
+ * For Java Everythin is Inside a Class. 
+ * 
+ *e.g Public Class Hello World { Every class has a name and we named this a class called Hellow World }
+ * 
+ * Also inside of classes we have methods.
+ * 
+ * e.g PubliceStaticVoidMain (String [] args){ 
+ * 
+ * System.out.printlin("Hello there!") <------------This is a Statemet which is also inclued  inside the 'main method.
+ * 
+ * } 
+ * 
+ * // String x = new String("Hello"); //property
+    //class - contains everything - has members.
+    //methods - Do something
+    //statements - Telling the computer t0 do something
+    //properties - store Something
+    //arguments - what you pass or call is to a method
+    //paramenter - when we def a method we give it a paramenter : or variables to store atguments 
+    //access modifier (public) - who can use and not use would be private.
+    //static - no intance of a class is needed 
+    //object - instance of a class
+    // (string [] args) this are known as arguments...=This helps to make our programs a little more dynamic.
+ */
+
+
+
+
+public class Helloworld {  // This is a class and this class name is called Hello World.
+
+
+    public static void main(String[] args) {  // Step #1 this is a method.  you can create an object with out using the static key world.
+        System.out.println("Emmanuel" ); //statement
+        System.out.println('U');
+        System.out.println("Isidahomen" );
+        //var x = new Helloworld(); // This is creating an object using step #2
+        //x.Greeting();
+    }
+     
+    
+   // public  void Greeting() {  // step #2 this method is created with out the static void and main.
+       // System.out.println(" my name is Emmanuel , Thank for asking!!");
+    }
+
+    
+
+//}
